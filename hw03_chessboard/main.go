4f99a55103d770fdfb5df518e9ee6cd861356d26
Angelica <angelikabuharova041101@gmail.com>
2023-09-27 19:44:10 +0300

    HW03

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,27 @@
 package main
 
+import "fmt"
+
 func main() {
-	// Place your code here.
+	var widht, heigth int
+	fmt.Scan(&widht, &heigth)
+	if widht == 0 {
+		return
+	}
+	for i := 0; i < heigth; i++ {
+		if i%2 == 0 {
+			fmt.Print("#")
+		}
+		for j := 0; j < widht; j++ {
+			if i%2 == 0 && j == widht-1 {
+				break
+			}
+			if j%2 == 0 {
+				fmt.Print(" ")
+			} else {
+				fmt.Print("#")
+			}
+		}
+		fmt.Println()
+	}
 }
