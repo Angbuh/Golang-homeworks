5ddc5cd0f69e6f312d2f14ff20ddeb3e59784a48
Angelica <angelikabuharova041101@gmail.com>
2023-11-08 10:58:07 +0300

    HW03(исправ)

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -3,17 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var widht, heigth int
-	fmt.Scan(&widht, &heigth)
-	if widht == 0 {
-		return
-	}
-	for i := 0; i < heigth; i++ {
+	var length int
+
+	fmt.Scan(&length)
+	for i := 0; i < length; i++ {
 		if i%2 == 0 {
 			fmt.Print("#")
 		}
-		for j := 0; j < widht; j++ {
-			if i%2 == 0 && j == widht-1 {
+		for j := 0; j < length; j++ {
+			if i%2 == 0 && j == length-1 {
 				break
 			}
 			if j%2 == 0 {
