71f7706c19631fd0623a44e4fd66abca30e5de4c
Angelica <angelikabuharova041101@gmail.com>
2024-01-16 14:02:39 +0300

    HW09

--- hw09_serialize/main.go
+++ hw09_serialize/main.go
@@ -1,36 +1,4 @@
 package main
 
-import (
-	"encoding/json"
-)
-
-// "fmt"
-
-type Book struct {
-	Id     int     `json:"id"`
-	Title  string  `json:"title"`
-	Author string  `json:"autor"`
-	Year   int     `json:"year"`
-	Size   int     `json:"size"`
-	Rate   float64 `json:"rate"`
-}
-
-func (b Book) MarshalJSON() ([]byte, error) {
-	return json.Marshal(b)
-}
-
-func (b *Book) UnmarshalJSON(data []byte) error {
-	return json.Unmarshal(data, b)
-}
-
 func main() {
-	book1 := &Book{
-		Id:     23,
-		Title:  "Tree",
-		Author: "Kate God",
-		Year:   1999,
-		Size:   10,
-		Rate:   7.5,
-	}
-
 }
